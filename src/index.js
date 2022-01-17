cdbe25544224ac58b6c697dc9fb054fc2e74495b
kyle-log <cocomo1316@gmail.com>
2022-01-17 13:52:10 +0900

    Change sqaure class to function

--- src/index.js
+++ src/index.js
@@ -2,14 +2,12 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 
-class Square extends React.Component {
-     render() {
-        return (
-            <button className="square" onClick={() => this.props.onClick()}>
-                {this.props.value}
-            </button>
-        );
-    }
+function Square(props) {
+    return (
+        <button className="square" onClick={props.onClick()}>
+            {props.value}
+        </button>
+    );
 }
 
 class Board extends React.Component {
@@ -27,6 +25,12 @@ class Board extends React.Component {
                 onClick={() => this.handleClick(i)}
             />
         );
+    }
+
+    handleClick(i) {
+        const squares = this.state.squares.slice();
+        squares[i] = 'X';
+        this.setState({squares: squares});
     }
 
     render() {
